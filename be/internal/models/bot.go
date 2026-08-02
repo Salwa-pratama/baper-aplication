d5ad8e68148cac41d2797a571558b11f05cbc534
Ananda <nandev1915@gmail.com>
2026-08-02 14:03:58 +0700

    benerin bot

--- be/internal/models/bot.go
+++ be/internal/models/bot.go
@@ -8,9 +8,6 @@ type Bot struct {
 	BusinessID          string    `gorm:"type:varchar(36);not null"`
 	Name                string    `gorm:"type:varchar(100);not null"`
 	WaStatus            string    `gorm:"type:varchar(50)"`
-	LangflowFlowID      string    `gorm:"type:varchar(100)"`
-	LangflowAPIEndpoint string    `gorm:"type:varchar(255)"`
-	LangflowAPIKey      string    `gorm:"type:varchar(255)"`
 	AgentPrompt         string    `gorm:"type:text"`
 	CreatedAt           time.Time
 	UpdatedAt           time.Time
